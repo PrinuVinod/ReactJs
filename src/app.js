d8bf7033e35d03c02afaf8c5d3dfa5b9904d2182
Prinu Vinod Nair <71308351+PrinuVinod@users.noreply.github.com>
2024-02-04 20:09:44 +0530

    node configs

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import HelloWorld from './components/HelloWorld';
+
+function App() {
+  return (
+    <div>
+      <h2>My React App</h2>
+      <HelloWorld />
+    </div>
+  );
+}
+
+export default App;
